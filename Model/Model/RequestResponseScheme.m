9064314b0195c0e6e0924dc0eadf67974705180d
liyuchang <liyuchang@jd.com>
2016-09-07 17:26:14 +0800

    123

--- Model/Model/RequestResponseScheme.m
+++ Model/Model/RequestResponseScheme.m
@@ -9,7 +9,13 @@
 #import "RequestResponseScheme.h"
 @implementation RequestResponseScheme
 
-
+/**
+ *  <#Description#>
+ *
+ *  @param data <#data description#>
+ *
+ *  @return <#return value description#>
+ */
 +(NSString *)responseURL:(NSDictionary *)data
 {
     NSDictionary  *temp = [NSDictionary dictionaryWithDictionary:data];
